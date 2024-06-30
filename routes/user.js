20fe8f17c1fbc73a5ce9f9e6e0d81c2903c2260d
vinaypanwar123 <vinaypanwar123s@gmail.com>
2024-06-30 22:47:16 +0530

    Update Routes

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,21 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const { signUp } = require("../controllers/user.js");
 const userController = require("../controllers/user.js");
+const listingController = require("../controllers/listings.js");
+const multer = require("multer");
 
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+router
+    .route("/")
+    .get(wrapAsync(listingController.index))
+    .post(
+      isLoggedIn,
+      validateListing,
+      upload.single("listing[image]"),
+      wrapAsync(listingController.createListing)
+    );
 router
   .route("/signup")
   .get(userController.renderSignUpForm)
@@ -23,5 +37,7 @@ router
     }),
     userController.login,
   );
+
 router.get("/logout", userController.logout);
+
 module.exports = router;
